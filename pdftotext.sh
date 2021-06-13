2b65a3492f5d29ad3d678dc7ab49ea97371d3b94
alb3rrt <alb3rrt@gmail.com>
2021-06-13 15:40:47 +0200

    pdftotext

---
+++ pdftotext.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# -*- ENCODING: UTF-8 -*#
+ 
+%https://pypi.org/project/pdftotext/
+%Convert *.pdf to plain text in a folder
+%Remove *.pdf files
+
+%Move to Main folder
+cd ArXiv_Papers/
+
+for file in *.pdf
+  do 
+    pdftotext "$file" "$file.txt"
+  done
+
+if [ "$1" = "rm" ]
+  then 
+    echo "Pdf removed"
+    rm *.pdf
+fi
+exit
--- script.sh
+++ script.sh
@@ -4,10 +4,9 @@
 %libreria origen utilizada https://github.com/MarkHershey/arxiv-dl
 
 %iteracion en la ID del paper en la
-%todos son de junio de 2021 (2106--> junio de 2021)
+%todos son de junio de 2021 2106--> junio de 2021
 
-
-for i in `seq 0000 5000`
+for i in `seq 0900 5000`
 do
   sleep 5s
   getpaper 2106.0$i
